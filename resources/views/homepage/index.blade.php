6464bb56183732ecfa7fe0f3cc1178e3feeaf64e
samibi6 <sambiname@yahoo.fr>
2023-11-17 17:03:35 +0100

    custom guest layout

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,6 +1,13 @@
-<h1>Liste des posts</h1>
-<ul>
-  @foreach($posts as $post)
-  <li>{{ $post->caption }}</li>
-  @endforeach
-</ul>
+<x-guest-layout>
+    <h1 class="font-bold text-xl mb-4">Liste des posts</h1>
+    <ul class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+        @foreach ($posts as $post)
+            <li>
+                <a class="flex bg-white rounded-md  shadow-md p-5 w-full hover:shadow-lg hover:scale-105 transition"
+                    href="#">
+                    {{ $post->caption }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="container mx-auto flex flex-col space-y-10">
+            <nav class="flex justify-between items-center py-2">
+                <div>
+                    <a href="/"
+                        class="group font-bold text-3xl flex items-center space-x-4 hover:text-emerald-600 transition ">
+                        <x-application-logo
+                            class="w-10 h-10 fill-current text-gray-500 group-hover:text-emerald-500 transition" />
+                        <span>Mon blog</span>
+                    </a>
+                </div>
+                <div class="flex items-center space-x-4 justify-end">
+                    <a class="font-bold hover:text-emerald-600 transition" href="/">Posts</a>
+                </div>
+            </nav>
+
+            <main>
                 {{ $slot }}
-            </div>
+            </main>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
